53cceae474683602d5f374cc3860a444f0e52346
Colette.Vitalis <brittanylasley@gmail.com>
2020-09-02 00:18:37 -0400

    test aga

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -6,3 +6,6 @@ home
 $('.carousel').carousel({
 		interval: 4000
 	});
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
