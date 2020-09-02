771ed648fabaea52b6a2c784a6ba7eaed966cb00
Jason Hescheles <jihescheles@gmail.com>
2020-09-02 18:13:09 -0400

    adding initial carousel

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -1,4 +1,25 @@
-var home = angular.module('home-controller', []);
+var home = angular.module('home-controller', ['ui.bootstrap']);
 home
 .controller('HomeController', ['$scope', function($scope){
+  $scope.interval = 6000;
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+  var currIndex = 0;
+  $scope.slides = [
+    {
+      image: 'assets/img/minecraft1.jpg',
+      text: 'minecraft1',
+      id: currIndex++
+    },
+    {
+      image: 'assets/img/minecraft2.jpg',
+      text: 'minecraft2',
+      id: currIndex++
+    },
+    {
+      image: 'assets/img/minecraft3.jpg',
+      text: 'minecraft3',
+      id: currIndex++
+    }
+  ];
 }]);
